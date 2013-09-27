8be83fe9595d4d86287aedacaab12af1ed699650
dbc-apprentice <dbc-apprentice@dbc.com>
2013-09-27 15:01:01 -0700

    .

--- flash_cards.rb
+++ flash_cards.rb
@@ -1,5 +1,17 @@
 
 # this is just a test to check if this gets saved to git hub
+
 class FileParser
+  
+  # file parser will take a file as input
+  # every pair of lines will get turned into a hash
+  # first row = hash[:term] = "term"
+  # second row = hash[:def] = "definition"
+  def self.parse(csv_file)
+    
+    CSV.foreach(csv_file) do |row|
+
+    end
+  end
 
 end
