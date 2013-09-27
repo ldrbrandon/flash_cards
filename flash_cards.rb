4d3a91c29f3deb61fbb347855f1c590fff5f4f49
dbc-apprentice <dbc-apprentice@dbc.com>
2013-09-27 16:02:43 -0700

    .

--- flash_cards.rb
+++ flash_cards.rb
@@ -1,17 +1,20 @@
-
 # this is just a test to check if this gets saved to git hub
 
 class FileParser
   
-  # file parser will take a file as input
-  # every pair of lines will get turned into a hash
-  # first row = hash[:term] = "term"
-  # second row = hash[:def] = "definition"
-  def self.parse(csv_file)
-    
-    CSV.foreach(csv_file) do |row|
-
+  def self.parse_file(file)
+    parsed_file_array = []
+    array_of_pairs = []
+    File.open(file).each do |row|
+      parsed_file_array << row.chomp 
     end
+    parsed_file_array.reject! { |item| item == " "} # remove elements that are " "
+    # create the array of pairs with every pair of elements being the values of a hash
+    parsed_file_array.each_slice(2) { |pair| array_of_pairs << {:term => pair[1], :definition => pair[0]} } 
+    return array_of_pairs
   end
 
 end
+
+
+FileParser.parse_file('flashcard_sample.txt')
